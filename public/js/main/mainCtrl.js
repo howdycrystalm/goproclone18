d6ce39dc3ae735c07f916f2aa9b1c77d001f0cab
Rasbandit <todd.rasband@gmail.com>
2016-11-29 14:57:30 -0700

    improved dropdown funcitonlity and created page indicators

--- public/js/main/mainCtrl.js
+++ public/js/main/mainCtrl.js
@@ -1,3 +1,12 @@
-angular.module("goApp").controller('mainCtrl', function($scope) {
-   $scope.test = 'Main CTRL works';
+angular.module("goApp").controller('mainCtrl', function($scope, $state, $timeout, $rootScope) {
+   $scope.stateName;
+   $scope.test = "this is a test"
+
+   $rootScope.$on('$stateChangeStart',
+      function(event, toState, toParams, fromState, fromParams){
+         $timeout(function() {
+           $scope.stateName = $state.current.name;
+           console.log($scope.stateName);
+        }, 100);
+      })
 });
