431510bc342b15fd67693c6e7838634c3ed6f2bd
Emme Gunther <Emme Gunther>
2016-12-06 11:51:28 -0700

    apparel changes

--- public/js/apparel/apparelCtrl.js
+++ public/js/apparel/apparelCtrl.js
@@ -1,16 +1,16 @@
 angular.module("goApp").controller('apparelCtrl', function($scope, mainService, products) {
 
-   $scope.products = products;
-   $scope.gender = 'male';
-   $scope.state = 1;
-   $scope.productFilter = function(product) {
-     if((product.gender === $scope.gender || product.gender === null) && product.state === $scope.state) {
-       console.log($scope.gender);
-       console.log(product);
-       return product;
-     }
+    $scope.products = products;
+    $scope.gender = 'male';
+    $scope.state = 1;
+    $scope.productFilter = function(product) {
+        if ((product.gender === $scope.gender || product.gender === null) && product.state === $scope.state) {
+            console.log($scope.gender);
+            console.log(product);
+            return product;
+        }
 
+        return false;
+    }
 
-     return false;
-     }
 });
