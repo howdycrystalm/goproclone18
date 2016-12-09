e8d942c91dd758ead66753d2b32d9d8306f572bf
howdycrystalm <howdycrystalm@gmail.com>
2016-12-08 19:46:07 -0700

    main image is showing in main image container

--- public/js/apparelProduct/apparelProductCtrl.js
+++ public/js/apparelProduct/apparelProductCtrl.js
@@ -1,18 +1,29 @@
 angular.module("goApp")
-    .controller('apparelProductCtrl', function($scope, apparelProductService, $stateParams) { //might not need ngAnimate
+    .controller('apparelProductCtrl', function($scope, apparelProductService, $stateParams, $timeout) { //might not need ngAnimate
 
         var id = $stateParams.id;
         var gender = $stateParams.gender;
         console.log(id + " " + gender);
         $scope.thumbs = [];
 
-        //originally tried passing params through function(), but params passed through fn have to be placeholder so instead passed through line 20
+        $scope.productImg = {};
+        $timeout(function() {
+                console.log($scope.productImg + " we've got this");
+            }, 100)
+            //originally tried passing params through function(), but params passed through fn have to be placeholder so instead passed through line 20
         $scope.getProduct = function() { //this fn automatically invokes because we need the obect on page load
                 apparelProductService.getProduct($stateParams.id, $stateParams.gender).then(function(response) { //we're able to pass $stateParams through this fn's params
                     $scope.product = response[0]; //save the response on scope, after figuring out what the pathways is via console log
-                    console.log($scope.product); //always console log your response
+                    $scope.productImg = response[0].img_url;
+                    console.log($scope.productImg + " I AM IMAGE");
                 })
             }() //<-- () automatically invokes
+
+        $scope.imgSwitch = function(theImage) {
+            apparelProductService.getProduct($stateParams.id, $stateParams.gender).then(function(response) { //we're able to pass $stateParams through this fn's params
+                $scope.productImg = response[0].img_url;
+            })
+        }
 
         $scope.getThumbs = function() {
             apparelProductService.getThumbs($stateParams.id, $stateParams.gender).then(function(response) {
@@ -24,6 +35,7 @@ angular.module("goApp")
                 console.log("this is $scope.thumbs", $scope.thumbs); //always console log your response
             })
         }()
+
 
 
 
