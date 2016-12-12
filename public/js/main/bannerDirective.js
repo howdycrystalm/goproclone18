292c60114a5df0cfbdc2e541ff54bbfc614a66f7
Emme Gunther <Emme Gunther>
2016-12-12 15:49:57 -0700

    redoing backend stuff and getting login username to display

--- public/js/main/bannerDirective.js
+++ public/js/main/bannerDirective.js
@@ -1,18 +1,20 @@
 angular.module('goApp').directive('banner', function() {
 
-   return {
-      restrict: 'E',
-      templateUrl: './views/bannerDir.html',
-      scope: {
-         state: '=',
-         products: '=',
-         subtot: '=',
-         incart: '='
-      },
-      controller: function($scope) {
-         $scope.hoverShop = false;
-         $scope.hoverWatch = false;
-         $scope.cartShow = false;
-      }
-   };
+    return {
+        restrict: 'E',
+        templateUrl: './views/bannerDir.html',
+        scope: {
+            state: '=',
+            products: '=',
+            subtot: '=',
+            incart: '=',
+            islogged: '=',
+            displayname: '='
+        },
+        controller: function($scope) {
+            $scope.hoverShop = false;
+            $scope.hoverWatch = false;
+            $scope.cartShow = false;
+        }
+    };
 })
