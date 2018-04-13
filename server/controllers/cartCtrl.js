c2109175302f2f1da4d1b3a6cd59ee6237d71770
howdycrystalm <howdycrystalm@gmail.com>
2018-04-12 22:53:11 -0700

    add to cart buttons on home view adding to cart successfully

--- server/controllers/cartCtrl.js
+++ server/controllers/cartCtrl.js
@@ -5,55 +5,57 @@ var stripeKey = require('./../stripeSecretKeys');
 var stripe = require('stripe')(stripeKey.secretKey);
 
 module.exports = {
+  //pull product from database to give cart info//
 
-    //pull product from database to give cart info//
+  getItem: function(req, res, next) {
+    if (req.params.id > 15) {
+      db.get_cart_product([req.params.id, req.params.prodid], function(
+        err,
+        product
+      ) {
+        res.status(200).json(product);
+      });
+    } else {
+      db.get_cameras([req.params.id], function(err, product) {
+        res.status(200).json(product);
+      });
+    }
+  },
 
-   getItem: function(req, res, next) {
-     if(req.params.id > 3) {
-        db.get_cart_product([req.params.id, req.params.prodid], function(err, product) {
-            res.status(200).json(product);
-        });
+  processPayment: function(req, res, next) {
+    //convert amount to pennies
+    var chargeAmt = req.body.amount;
+    var amountArray = chargeAmt.toString().split('');
+    var pennies = [];
+    for (var i = 0; i < amountArray.length; i++) {
+      if (amountArray[i] === '.') {
+        if (typeof amountArray[i + 1] === 'string') {
+          pennies.push(amountArray[i + 1]);
+        } else {
+          pennies.push('0');
+        }
+        if (typeof amountArray[i + 2] === 'string') {
+          pennies.push(amountArray[i + 2]);
+        } else {
+          pennies.push('0');
+        }
+        break;
+      } else {
+        pennies.push(amountArray[i]);
       }
-      else {
-         db.get_cameras([req.params.id], function(err, product) {
-            res.status(200).json(product);
-         })
+    }
+    const convertedAmt = parseInt(pennies.join(''));
+
+    var charge = stripe.charges.create(
+      {
+        amount: convertedAmt, // amount in cents, again
+        currency: 'usd',
+        source: req.body.payment.token,
+        description: 'Test charge for Go.com'
+      },
+      function(err, charge) {
+        res.sendStatus(200);
       }
-    },
-
-   processPayment: function(req, res, next) {
-
-     //convert amount to pennies
-     var chargeAmt = req.body.amount;
-     var amountArray = chargeAmt.toString().split('');
-     var pennies = [];
-     for (var i = 0; i < amountArray.length; i++) {
-         if (amountArray[i] === ".") {
-             if (typeof amountArray[i + 1] === "string") {
-                 pennies.push(amountArray[i + 1]);
-             } else {
-                 pennies.push("0");
-             }
-             if (typeof amountArray[i + 2] === "string") {
-                 pennies.push(amountArray[i + 2]);
-             } else {
-                 pennies.push("0");
-             }
-             break;
-         } else {
-             pennies.push(amountArray[i])
-         }
-     }
-     const convertedAmt = parseInt(pennies.join(''));
-
-     var charge = stripe.charges.create({
-         amount: convertedAmt, // amount in cents, again
-         currency: 'usd',
-         source: req.body.payment.token,
-         description: 'Test charge for Go.com'
-     }, function(err, charge) {
-         res.sendStatus(200);
-     });
-   }
-
+    );
+  }
 };
